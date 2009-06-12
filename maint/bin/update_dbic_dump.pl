ee25713d6e0deaa15c17c43719fd8cd6aa2eeec4
Robert Buels <rmb32@cornell.edu>
2009-06-12 16:11:39 -0700

    update_dbic_dump.pl outline in place

--- maint/bin/update_dbic_dump.pl
+++ maint/bin/update_dbic_dump.pl
@@ -10,33 +10,49 @@ use English;
 use Carp;
 use FindBin;
 
-use Getopt::Std;
+use Getopt::Long;
 use Pod::Usage;
 
 #use Data::Dumper;
 
-our %opt;
-getopts('',\%opt) or pod2usage(1);
+
+## parse and validate command-line options
+GetOptions(
+          )
+    or pod2usage(1);
+
+## check out a new chado schema copy
 
 
-
-
-
-
-
-
-
+# parse the modules definition
+# drop all tables from the target database
+# traverse modules in dependency order
+#   list the current set of tables and views
+#   load the module into the database
+#   find what tables and views are new
+#   do a make_schema_at, restricted to the new set of tables and views,
+#       dumping to Chado::Schema::ModuleName::ViewOrTableName
 
 
 __END__
 
 =head1 NAME
 
-update_dbic_dump.pl - script to do something
+update_dbic_dump.pl - script to sync the DBIx::Class object layer with
+the current set of chado tables
+
+=head1 DESCRIPTION
+
+This script basically:
+
+  - check out a clean chado schema copy
+  - drop all tables from the target database
+  - make and install it with all modules
+  - do a make_schema_at on it to refresh modules
 
 =head1 SYNOPSIS
 
-  update_dbic_dump.pl [options] args
+  update_dbic_dump.pl [options] chado_version
 
   Options:
 
