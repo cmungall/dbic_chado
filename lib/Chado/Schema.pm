5a25c304cf788ed6c7130993af298caffa2a3847
Robert Buels <rmb32@cornell.edu>
2009-08-15 16:45:11 -0700

    added some POD to Chado::Schema

--- lib/Chado/Schema.pm
+++ lib/Chado/Schema.pm
@@ -14,3 +14,49 @@ __PACKAGE__->load_classes;
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
+
+
+__END__
+
+=head1 NAME
+
+Chado::Schema - standard DBIx::Class layer for the Chado database schema
+
+=head1 SYNOPSIS
+
+  use Chado::Schema;
+
+  my $chado = Chado::Schema->connect($dsn,$user,$password);
+
+  print "number of rows in feature table: ",
+        $chado->resultset('Sequence::Feature')->count,
+        "\n";
+
+
+=head1 DESCRIPTION
+
+This is a standard object-relational mapping layer for use with the
+GMOD Chado database schema.  This layer is implemented with
+L<DBIx::Class>, generated with the help of the very fine
+L<DBIx::Class::Schema::Loader> module.
+
+To learn how to use this L<DBIx::Class> ORM layer, a good starting
+point is the L<DBIx::Class::Manual>.
+
+=head1 CONTRIBUTORS
+
+Robert Buels, E<lt>rmb32@cornell.eduE<gt>
+
+=head1 AUTHOR
+
+Robert Buels, E<lt>rmb32@cornell.eduE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Boyce Thompson Institute for Plant Research
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
