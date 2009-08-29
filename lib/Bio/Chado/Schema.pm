41d621331085efe0262f30b132de68a5afb4e4ae
Robert Buels <rmb32@cornell.edu>
2009-08-29 13:19:01 -0700

    moved version number into Bio::Chado::Schema to appease cpan indexer

--- lib/Bio/Chado/Schema.pm
+++ lib/Bio/Chado/Schema.pm
@@ -11,6 +11,9 @@ __PACKAGE__->load_classes;
 # Created by DBIx::Class::Schema::Loader v0.04999_07 @ 2009-08-29 09:17:46
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:G1fnTOuqELdY0CYrwKEDsA
 
+our $VERSION = '0.03';
+$VERSION = eval $VERSION;
+
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
