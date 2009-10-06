edf2d7307916e9116fec4f2663b1ad54723742e2
Robert Buels <rmb32@cornell.edu>
2009-10-06 09:55:56 -0700

    removed addition of explicit column to General::Dbxref

--- lib/Bio/Chado/Schema/General/Dbxref.pm
+++ lib/Bio/Chado/Schema/General/Dbxref.pm
@@ -159,13 +159,5 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.04999_07 @ 2009-08-31 08:24:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tNdwAyrG6nfrC6C2aoAfPw
 
-# add an explicit sequence name to the dbxref
-__PACKAGE__->add_columns(
-  'dbxref_id',
-   { %{ __PACKAGE__->column_info('dbxref_id') },
-     sequence => 'dbxref_dbxref_id_seq',
-   }
-  );
-
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
