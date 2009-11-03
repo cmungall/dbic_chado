78cbf04119614d2c0ed00312152e55d022c9e85d
Robert Buels <rmb32@cornell.edu>
2009-11-03 11:17:47 -0800

    moved Util.pm functions into Schema.pm file to prevent them being found by Module::Find

--- lib/Bio/Chado/Schema.pm
+++ lib/Bio/Chado/Schema.pm
@@ -1,3 +1,14 @@
+##################################################
+##                                              ##
+## IMPORTANT NOTE about this file:              ##
+##                                              ##
+## This file containts two different packages   ##
+##   + Bio::Chado::Schema                       ##
+##   + Bio::Chado::Schema::Util                 ##
+##                                              ##
+##################################################
+
+
 package Bio::Chado::Schema;
 
 use strict;
@@ -5,40 +16,16 @@ use warnings;
 
 use base 'DBIx::Class::Schema';
 
-require Bio::Chado::Schema::Util; #< load Util class for use by Schema
-                                  #resultsources
-require Module::Find;
-
-my @components =
-
-    # skip loading modules that are not DBIC components currently just
-    # skips Bio::Chado::Schema::Util
-    grep $_ ne 'Util',
-
-    # trim off this package name to leave just the part under this
-    # namespace
-    map { substr $_, length __PACKAGE__.'::' } 
-
-
-    # find all the modules under the namespace of this package
-    # (i.e. Bio::Chado::Schema::*)
-    Module::Find::findallmod(__PACKAGE__);
-
-__PACKAGE__->load_classes(@components);
-
+__PACKAGE__->load_classes();
 
 # Created by DBIx::Class::Schema::Loader v0.04999_07 @ 2009-08-31 08:24:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IHy0lr/uwG8dgRR2sDOVoQ
 
-our $VERSION = '0.04100';
+our $VERSION = '0.04200';
 $VERSION = eval $VERSION;
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
-1;
-
-
-__END__
 
 =head1 NAME
 
@@ -84,3 +71,152 @@ it under the same terms as Perl itself.
 
 =cut
 
+
+#########################################################################################
+##                                                                                     ##
+## NOTE: The documentation for this package can be found in Bio/Chado/Schema/Util.pod  ##
+##                                                                                     ##
+## The reason to put this package in the same file than Schema.pm is because if you    ##
+## put non DBIx::Class packages under Schema folder, when Module::Find get all the     ##
+## dbix classes to load in the schema, the non dbix classes will return an error       ##
+##                                                                                     ##
+#########################################################################################
+
+package Bio::Chado::Schema::Util;
+use strict;
+use Carp::Clan qr/^Bio::Chado::Schema/;
+
+
+sub create_properties {
+    my ($class,  %args) = @_;
+
+    #check for required args
+    $args{prop_relation_name} or confess "must provide $_ arg"
+      for qw/ row prop_relation_name properties options/;
+
+    my $self  = delete $args{row};
+    my $props = delete $args{properties};
+    my $opts  = delete $args{options};
+    my $prop_relation_name = delete $args{prop_relation_name};
+
+    %args and confess "invalid option(s): ".join(', ', sort keys %args);
+
+    # normalize the props to hashrefs
+    foreach (values %$props) {
+        $_ = { value => $_ } unless ref eq 'HASH';
+    }
+
+    # process opts
+    $opts ||= {};
+    defined $opts->{cv_name} or confess 'must provide a cv_name in options';
+    $opts->{db_name} = 'null'
+        unless defined $opts->{db_name};
+    $opts->{dbxref_accession_prefix} = 'autocreated:'
+        unless defined $opts->{dbxref_accession_prefix};
+
+    my $schema = $self->result_source->schema;
+
+    my $prop_cv = do {
+        my $cvrs = $schema->resultset('Cv::Cv');
+        my $find_or_create = $opts->{autocreate} ? 'find_or_create' : 'find';
+        $cvrs->$find_or_create({ name => $opts->{cv_name}},
+                               { key => 'cv_c1' })
+            or croak "cv '$opts->{cv_name}' not found and autocreate option not passed, cannot continue";
+    };
+
+    my $prop_db; #< set as needed below
+
+    # find/create cvterms and dbxrefs for each of our featureprops,
+    # and remember them in %propterms
+    my %propterms;
+    foreach my $propname (keys %$props) {
+        my $existing_cvterm = $propterms{$propname} =
+            $prop_cv->find_related('cvterms',
+                                   { name => $propname,
+                                     is_obsolete => 0,
+                                   },
+                                   { key => 'cvterm_c1' },
+                                  );
+
+        # if there is no existing cvterm for this in the prop table,
+        # and we have the autocreate flag set true, then create a
+        # cvterm, dbxref, and db for it if necessary
+        unless( $existing_cvterm ) {
+            $opts->{autocreate}
+               or croak "cvterm not found for property '$propname', and autocreate option not passed, cannot continue";
+
+            # look up the db object if we don't already have it, now
+            # that we know we need it
+            $prop_db ||=
+                $self->result_source->schema
+                     ->resultset('General::Db')
+                     ->find_or_create( { name => $opts->{db_name} },
+                                       { key => 'db_c1' }
+                                     );
+
+            # find or create the dbxref for this cvterm we are about
+            # to create
+            my $dbx_acc = $opts->{dbxref_accession_prefix}.$propname;
+            my $dbxref =
+                $prop_db->search_related( 'dbxrefs',
+                                          { accession => $dbx_acc },
+                                          { order_by => { -desc => ['version'] } }
+                                        )
+                                ->first
+             || $prop_db->create_related( 'dbxrefs', { accession => $dbx_acc,
+                                                               version => 1,
+                                                             });
+
+            # look up any definition we might have been given for this
+            # propname, so we can insert it if given
+            my $def = $opts->{definitions}->{$propname};
+
+            $propterms{$propname} =
+                $prop_cv->create_related('cvterms',
+                                         { name => $propname,
+                                           is_obsolete => 0,
+                                           dbxref_id => $dbxref->dbxref_id,
+                                           $def ? (definition => $def) : (),
+                                         }
+                                        );
+        }
+    }
+
+    my %props;
+    while( my ($propname,$propval) = each %$props ) {
+
+        my $data = ref $propval
+            ? {%$propval}
+            : { value => $propval };
+
+        $data->{type_id} = $propterms{$propname}->cvterm_id;
+
+
+	# decide whether to skip creating this prop
+	my $skip_creation = $opts->{allow_duplicate_values}
+            ? 0
+            : $self->search_related( $prop_relation_name,
+                                     { type_id => $data->{type_id},
+                                       value   => $data->{value},
+                                     })
+                   ->count;
+
+
+        unless( $skip_creation ) {
+            # find highest rank for props of this type
+            my $max_rank= $self->search_related( $prop_relation_name,
+                                                 { type_id =>$data->{type_id} }
+                                               )->get_column('rank')->max;
+            $data->{rank} = defined $max_rank ? $max_rank + 1 : 0;
+
+	    $props{$propname} = $self->create_related( $prop_relation_name,
+						       $data
+                                                     );
+	}
+    }
+    return \%props;
+}
+
+###
+1;#
+###
