ba3dc306123a3d3283c3d567e92d026a4497a8fa
Naama Menda <nm249@cornell.edu>
2010-01-30 03:40:28 -0500

    added tests for Cvterm functions

--- lib/Bio/Chado/Schema/Cv/Cvterm.pm
+++ lib/Bio/Chado/Schema/Cv/Cvterm.pm
@@ -1446,6 +1446,22 @@ __PACKAGE__->has_many(
 
 use Carp;
 
+
+
+=head2 cvtermsynonyms
+
+Type: has_many
+
+Related object: L<Bio::Chado::Schema::Cv::Cvtermsynonym>
+
+=cut
+__PACKAGE__->has_many(
+  "cvtermsynonyms",
+  "Bio::Chado::Schema::Cv::Cvtermsynonym",
+  { "foreign.cvterm_id" => "self.cvterm_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 =head2 add_synonym
 
  Usage:        $self->add_synonym($synonym , { type => 'exact' , autocreate => 1} );
@@ -1541,15 +1557,17 @@ sub add_synonym {
             my $def = $opts->{definition};
 
 
-	    my $synonym_type_cvterm= $synonym_cv->create_related('cvterms',
+	    my $synonym_type_cvterm= $synonym_cv->create_related('types',
 								 { name => $synonym_type,
 								   is_obsolete => 0,
 								   dbxref_id => $dbxref->dbxref_id,
 								   $def ? (definition => $def) : (),
 								 }
 		);
-	    $data->{type_id} = $synonym_type_cvterm->cv_id();
-        }
+	    $data->{type_id} = $synonym_type_cvterm->cvterm_id();
+        } else {
+	    $data->{type_id} = $existing_cvterm->cvterm_id();
+	}
     }
     my ($cvtermsynonym)= $self->search_related('featureprops',
 					       {type_id => $data->{type_id},
@@ -1608,11 +1626,8 @@ sub get_secondary_dbxrefs {
     my @list;
     my @s =  $self->search_related('cvterm_dbxrefs' , { is_for_definition => 0} );
     foreach (@s) {
-	my ($db_name, $accession)  = $schema->resultset("General::Dbxref")->search(
-	    { dbxref_id => $_->get_column('dbxref_id') },
-	    { join      => { 'db' => 'db_id' },
-	      +select   => [ 'db.name', 'dbxref.accession' ],
-	    } );
+	my $accession = $_->dbxref->accession;
+	my $db_name = $_->dbxref->db->name;
 	push @list, $db_name . ":" .  $accession;
     }
     return @list;
@@ -1632,10 +1647,11 @@ sub get_secondary_dbxrefs {
 
 sub add_secondary_dbxref {
     my ($self, $accession, $def)=@_;
-    $def = 0 unless $def == 1;
+    $def = 0 if !$def;
+    
     my $schema = $self->result_source->schema;
     my ($db_name, $acc) = split (/:/, $accession);
-    if (!$db_name || $accession) { croak "Did not pass a legal accession! ($accession)" ; }
+    if (!$db_name || !$acc) { croak "Did not pass a legal accession! ($accession)" ; }
     my $db = $schema->resultset("General::Db")->find_or_create(
 	{ name => $db_name },
 	{ key => 'db_c1' }
@@ -1675,7 +1691,7 @@ sub delete_secondary_dbxref {
     my $accession=shift;
     my $schema = $self->result_source->schema;
     my ($db_name, $acc) = split (/:/, $accession);
-    if (!$db_name || $accession) { croak "Did not pass a legal accession! ($accession)" ; }
+    if (!$db_name || !$accession) { croak "Did not pass a legal accession! ($accession)" ; }
 
     my ($cvterm_dbxref) = $schema->resultset("General::Db")->search(
 	{ name => $db_name } )->
@@ -1744,19 +1760,23 @@ sub create_with {
 				      ->find_or_create({ name => $opts->{cv} });
 
     # return our cvterm if it exists already
-    if( my $cvterm = $cv->find_related( 'cvterms', { name => $opts->{name} }) ) {
+    if( my $cvterm = $cv->find_related( 'cvterms', 
+					{ 
+					    name => $opts->{name},
+					    is_obsolete => '0',
+					}) ) {
 	return $cvterm;
     }
-
+    
     # now figure out which dbxref to use (creating the dbxref and db if necessary)
     my $dbx = _find_dbxref( $schema, $opts->{dbxref}, $opts->{db} );
-
+    
     # and finally make a cvterm to go with the cv and dbxref we found
     return $cv->create_related( 'cvterms',
 				{ name => $opts->{name},
 				  dbxref_id => $dbx->dbxref_id,
 			        }
-			      );
+	);
 }
 sub _find_dbxref {
     my ( $schema, $dbx, $db ) = @_;
--- t/Cv/010_cvterm.t
+++ t/Cv/010_cvterm.t
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 
-use Test::More tests => 4;
+use Test::More tests => 13;
 use Test::Exception;
 
 
@@ -32,6 +32,40 @@ SKIP: {
         is( $cvterm->dbxref->db->name,
 	    'null',
 	    'db name autocreated test' );
+	
+	#now add a synonym 
+	my $synonym='test synonym';
+	my $type='exact';
+	my $cvtermsynonym= $cvterm->add_synonym($synonym, { synonym_type=>$type, autocreate=>1 });
+	
+	is($cvtermsynonym->synonym, $synonym, "synonym test");
+	is($cvtermsynonym->cvterm->name, $name, "synonym type test");
+	is($cvtermsynonym->type->cv->name, 'synonym_type', "synonym cv name test");
+	is($cvtermsynonym->type->dbxref->accession, $type, "synonym dbxref accession test");
+	
+	##delete the synonym
+	$cvterm->delete_synonym($synonym);
+	
+	is($cvterm->search_related('cvtermsynonyms', {synonym=>$synonym } )->count , 0, "deleted synonym test");
+
+	#add a secondary dbxref
+	my $sec_db = 'SEC';
+	my $sec_acc= '1234';
+	$cvterm->add_secondary_dbxref($sec_db.":".$sec_acc);
+	
+	my ($cvterm_dbxref)= $cvterm->get_secondary_dbxrefs;
+	my ($re_db, $re_acc) = split(  ":" , $cvterm_dbxref); 
+	is($re_db, $sec_db, "secondary dbxref db name test");
+	is($re_acc, $sec_acc, "secondary dbxref accession test");
+
+	#and delete the secondary dbxref
+	$cvterm->delete_secondary_dbxref($sec_db.":".$sec_acc);	
+
+	($cvterm_dbxref)= $cvterm->get_secondary_dbxrefs;
+	is($cvterm_dbxref, undef, "deleted secondary dbxref test");
+	is($cvterm->search_related('cvterm_dbxrefs' )->count , 0, "deleted cvterm_dbxref test");
+	
+	
 	$schema->txn_rollback;
     });
 }
