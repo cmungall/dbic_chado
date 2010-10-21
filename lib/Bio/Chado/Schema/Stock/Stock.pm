3447c54fcbdb235ed31b3f149a0860cd735a8ce3
Naama Menda <nm249@cornell.edu>
2010-10-21 04:15:38 -0400

    fixed POD for create_stockprops

--- lib/Bio/Chado/Schema/Stock/Stock.pm
+++ lib/Bio/Chado/Schema/Stock/Stock.pm
@@ -319,7 +319,7 @@ __PACKAGE__->has_many(
 =head2 create_stockprops
 
   Usage: $set->create_stockprops({ baz => 2, foo => 'bar' });
-  Desc : convenience method to create stock properties using stocks
+  Desc : convenience method to create stock properties using cvterms
           from the ontology with the given name
   Args : hashref of { propname => value, ...},
          options hashref as:
@@ -338,9 +338,9 @@ __PACKAGE__->has_many(
             dbxref_accession_prefix => optional, default
                                        'autocreated:',
             definitions => optional hashref of:
-                { stock_name => definition,
+                { cvterm_name => definition,
                 }
-             to load into the stock table when autocreating stocks
+             to load into the cvterm table when autocreating cvterms
 
              rank => force numeric rank. Be careful not to pass ranks that already exist
                      for the property type. The function will die in such case.
